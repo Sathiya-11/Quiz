6634779dad676a97d2f4a2024a04da67c572ae5c
bamasathiya37@gmail.com <bamasathiy37@gmail.com>
2025-07-15 19:05:09 +0530

    Initial commit with project files

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+ import App from './App';
+// import HotelMenu from './components/task.jsx'
+
+ const root = ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+// )
+// createRoot(document.getElementById('root1')).render(
+//   <HotelMenu />
+ );
